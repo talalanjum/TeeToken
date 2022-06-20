56a825431481eb75313d33b53f7eed14feea3bb7
Talal <muhammaadtalal@gmail.com>
2022-06-20 01:45:08 +0100

    Fixed get_account function

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,6 +1,7 @@
 from brownie import (
     accounts,
-    network
+    network,
+    config
 )
 
 FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev", "rinkeby"]
@@ -8,13 +9,12 @@ LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
 
 def get_account(index=None, id=None):
-    print(accounts[0])
     if index:
         return accounts[index]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        return accounts[0]
     if id:
         return accounts.load(id)
-    if (
-        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
-        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
-    ):
-        return accounts[0]
+    if network.show_active() in config["networks"]:
+        return accounts.add(config["wallets"]["from_key"])
+    return None
